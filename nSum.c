4f2eb76e0195fada995c5c0bf1672df7d1df279e
igoraserpac <igoraserpac@gmail.com>
2024-04-26 14:28:15 -0300

    nSum

---
+++ nSum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Problem: Find the sum of the first N numbers.
+
+    Objective function: f(i) is the sum of the first i natural numbers
+
+    Recurrence relation: f(n) = f(n-1) + n
+*/
+
+int nSum(int n){
+    int *vector = (int*)malloc((n + 1) * sizeof(int));
+    vector[0] = 0;
+    for(int i = 0; i < n+1; i++){
+        vector[i] = vector[i-1] + i;
+    }
+    return vector[n];
+}
+
+int main(){
+    printf("sum of the first 1000 numbers: %d\n", nSum(1000));
+}
