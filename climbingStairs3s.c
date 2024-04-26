1ff5357891439bb2e44b5252e46c51c299c345df
igoraserpac <igoraserpac@gmail.com>
2024-04-26 15:28:21 -0300

    climbingStairs3s

--- climbingStairs3s.c
+++ climbingStairs3s.c
@@ -5,14 +5,14 @@
         Climbing Stairs
 
         You are climbing a stair case. It takes n steps to reach the top.
-        Each time you can either climb 1 or 2 steps.
+        Each time you can either climb 1, 2 or 3 steps.
         In how many distincts ways can you climb to the top?
 
     Objective function: 
         f(i) is the number of distinct way to climb to the i-th stair.
 
     Recurrence relation: 
-        f(n) = f(n-1) + f(n-2)
+        f(n) = f(n-1) + f(n-2) + f(n-3)
 */
 
 long int climbingStairs(long int n){
@@ -20,8 +20,9 @@ long int climbingStairs(long int n){
     long int vector[n+1];
     vector[0] = 1;
     vector[1] = 1;
-    for(long int i = 2; i < n+1; i++){
-        vector[i] = vector[i-1] + vector[i-2];
+    vector[2] = 2;
+    for(long int i = 3; i < n+1; i++){
+        vector[i] = vector[i-1] + vector[i-2] + vector[i-3];
     }
     return vector[n];
 }
@@ -30,13 +31,15 @@ long int improvedClimbingStairs(long int n){
     if(n < 2) return 1;
     int a = 1;
     int b = 1;
-    int c;
-    for(long int i = 2; i < n+1; i++){
-        c = a + b;
+    int c = 2;
+    int d;
+    for(long int i = 3; i < n+1; i++){
+        d = a + b + c;
         a = b;
         b = c;
+        c = d;
     }
-    return c;
+    return d;
 }
 
 int main(){
